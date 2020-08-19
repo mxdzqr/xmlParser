3b37e68a1e285bcbe59f9c17502fd8e6f29f7286
mxdzqr <mxdzqr@gmail.com>
2020-08-19 04:49:47 +0200

    add output xml->json

--- main.go
+++ main.go
@@ -1,63 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"github.com/gorilla/mux"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
+
+//type Rates struct {
+//	XMLName xml.Name `xml:"rates"`
+//	Rates   []Item   `xml:"item"`
+//}
+//
+//type Item []struct {
+//	XMLName xml.Name `xml:"item"`
+//	From    string   `xml:"from"`
+//	To string `xml:"to"`
+//	In string `xml:"in"`
+//	Out string `xml:"out"`
+//	Amount string `xml:"amount"`
+//	Minamount string `xml:"minamount"`
+//	Maxamount string `xml:"maxamount"`
+//	Param string `xml:"param"`
+//	City string `xml:"city"`
+//}
+
 type Rates struct {
-	XMLName xml.Name `xml:"rates"`
-	Rates   []Item   `xml:"item"`
-}
+	Items  []Item `xml:"item"`
 
+}
 type Item struct {
-	XMLName xml.Name `xml:"item"`
-	From    string   `xml:"from"`
-	To string `xml:"to"`
-	In string `xml:"in"`
-	Out string `xml:"out"`
-	Amount string `xml:"amount"`
-	Minamount string `xml:"minamount"`
-	Maxamount string `xml:"maxamount"`
-	Param string `xml:"param"`
-	City string `xml:"city"`
+	From      string   `xml:"from"`
+	To        string   `xml:"to"`
+	In        string   `xml:"in"`
+	Out       string   `xml:"out"`
+	Amount    string   `xml:"amount"`
+	Minamount string   `xml:"minamount"`
+	Maxamount string   `xml:"maxamount"`
+	Param     string   `xml:"param"`
+	City      string   `xml:"city"`
+}
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/stop", StopServer)
+	router.HandleFunc("/courses", Courses)
+	log.Fatal(http.ListenAndServe(":80", router))
 }
 
+func Courses(w http.ResponseWriter, r *http.Request) {
+	resp, _ := http.Get("https://test.cryptohonest.ru/request-exportxml.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	var s Rates
+	xml.Unmarshal(bytes, &s)
+	b, err := json.Marshal(s.Items)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	w.Write([]byte(b))
+}
 
 func StopServer(w http.ResponseWriter, r *http.Request) {
 	os.Exit(0)
 }
 
-func Courses(w http.ResponseWriter, r *http.Request) {
-	xmlValue, err := http.Get("https://test.cryptohonest.ru/request-exportxml.xml")
 
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer xmlValue.Body.Close()
-	fmt.Println("Success, was read link")
-	body, _ := ioutil.ReadAll(xmlValue.Body)
-	var rates Rates
-	xml.Unmarshal(body, &rates)
-	for i := 0; i < len(rates.Rates); i++ {
-		fmt.Fprint(w, rates.Rates[i])
-	}
-}
-
-func main() {
-
-	router := mux.NewRouter()
-	router.HandleFunc("/stop", StopServer)
-	router.HandleFunc("/courses", Courses)
-	http.Handle("/", router)
-
-	fmt.Println("Server is start..")
-	http.ListenAndServe(":80", nil)
-
-
-}
